f8a27b9812c214960bde2055d0c2c17f93480c7d
Giancarlo Noseda <g.noseda.soto@gmail.com>
2020-11-19 14:57:56 -0300

    edit routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   root 'books#index'
 
+  #resources :books
   get 'books', to: 'books#index'
 
   get 'books/new', to: 'books#new' #creacion de libros
   post 'books', to: 'books#create' #pantalla posterior a creacion de libros
   
-  get 'books/:id', to: 'books#show' #menú de libros
+  get 'books/:id', to: 'books#show', as: 'book' #menú de libros
   
-  get 'books/:id/edit', to: 'books#edit' #Edición de libros y estados
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book' #Edición de libros y estados
   patch 'books/:id', to: 'books#update'  #pantalla psoterior a libros y estados
+  put 'books/:id', to: 'books#update'  #pantalla psoterior a libros y estados
   
+
   delete 'books/:id', to: 'books#destroy' #destrucción de libros
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,29 +1,46 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get books_new_url
+  test 'book create' do
+    
+    30.times do |i|
+      params = {
+        title: "Title " + i.to_s,
+        author: "Autor " + i.to_s,
+        status: :Guardado,
+        lend_time: DateTime.now,
+        return_time: DateTime.now,
+        lend_to: "Nobody"
+      }
+       
+      post '/books', params: {book: params}
+    end 
     assert_response :success
   end
 
-  test "should get create" do
-    get books_create_url
-    assert_response :success
-  end
+#test "should get new" do
+  ##  get books_new_url
+  #  assert_response :success
+  #end
 
-  test "should get edit" do
-    get books_edit_url
-    assert_response :success
-  end
+  #test "should get create" do
+  ##  get books_create_url
+   # assert_response :success
+  #end
 
-  test "should get update" do
-    get books_update_url
-    assert_response :success
-  end
+  #test "should get edit" do
+  #  get books_edit_url
+  #  assert_response :success
+  #end
 
-  test "should get destroy" do
-    get books_destroy_url
-    assert_response :success
-  end
+  #test "should get update" do
+  #  get books_update_url
+  #  assert_response :success
+  #end
+
+  #test "should get destroy" do
+  #  get books_destroy_url
+  #  assert_response :success
+  #end
 
 end
